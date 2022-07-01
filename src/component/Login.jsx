173039e33538658e256eec4a1191e5a47d8c9234
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-07-01 23:34:02 +0530

    working on login page

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "../css/login.css";
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="login-container">
+        <img src="./whatsapp-logo.png" alt="" />
+        <p className="login-name">WhatsApp Web</p>
+        <button className="login-btn">
+          <img src="./google-logo.png" alt="" />
+          Login with Google
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
