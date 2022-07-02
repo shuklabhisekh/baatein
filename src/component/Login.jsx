cdda120fbfda00793a6cdef99c8a475cef117e86
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-07-02 12:19:35 +0530

    working on login page

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import "../css/login.css";
 import { useNavigate } from "react-router-dom";
-import db, { auth, googleProvider } from "../Firebase";
+// import db, { auth, googleProvider } from "../firebase";
 function Login({ setUser }) {
   const navigate = useNavigate();
   const signInWithGoogle = () => {
-    auth
-      .signInWithPopup(googleProvider)
-      .then((res) => {
-        const newUser = {
-          fullname: res.user.displayName,
-          email: res.user.email,
-          photoURL: res.user.photoURL,
-        };
-        navigate("/");
-        setUser(newUser);
-        db.collection("users").doc(res.user.email).set(newUser);
-      })
-      .catch((err) => alert(err));
+    console.log("clicked");
+    // auth
+    //   .signInWithPopup(googleProvider)
+    //   .then((res) => {
+    //     const newUser = {
+    //       fullname: res.user.displayName,
+    //       email: res.user.email,
+    //       photoURL: res.user.photoURL,
+    //     };
+    //     navigate("/");
+    //     setUser(newUser);
+    //     db.collection("users").doc(res.user.email).set(newUser);
+    //   })
+    //   .catch((err) => alert(err));
   };
   return (
     <div className="login">
